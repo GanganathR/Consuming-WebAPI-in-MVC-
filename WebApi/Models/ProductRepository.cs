bde4b3b98ec5718c390f86b7aa58176adc5d841b
kgr87 <kasungr@gmail.com>
2017-10-07 23:23:21 +0530

    add files

---
+++ WebApi/Models/ProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models
+{
+    public class ProductRepository : IProduct
+    {
+        private List<Product> listProducts = new List<Product>();
+        public ProductRepository()
+        {
+            listProducts.Add(new Product { ID = "p1", Name = "aaaa", Price = 100 });
+            listProducts.Add(new Product { ID = "p2", Name = "bbbb", Price = 200 });
+        }
+        public Product FindbyId(string id)
+        {
+            return listProducts.Single(p =>p.ID.Equals(id));
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return listProducts;
+        }
+    }
+}
